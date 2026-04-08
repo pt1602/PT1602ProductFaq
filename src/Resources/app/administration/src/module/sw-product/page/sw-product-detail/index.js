5756f362ac63fefbdb3a1716526944e18a0eaf08
Philipp Tuchardt <philipp.tuchardt@8mylez.com>
2026-04-08 09:05:48 +0200

    feat(admin): add module

---
+++ src/Resources/app/administration/src/module/sw-product/page/sw-product-detail/index.js
@@ -0,0 +1,15 @@
+import template from './sw-product-detail.html.twig';
+
+const { Component } = Shopware;
+
+Component.override('sw-product-detail', {
+    template,
+
+    computed: {
+        productCriteria() {
+            const criteria = this.$super('productCriteria');
+            criteria.addAssociation('pt1602ProductFaq');
+            return criteria;
+        }
+    }
+}); 
